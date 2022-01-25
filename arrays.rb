8e05a0a328ba9ca7601d0f25c2a38d00cbb1b5e4
reuben <creubend@gmail.com>
2022-01-25 14:25:19 -0700

    Added methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,12 @@
 names = ["Ruby", "Fred", "Billy", "Bob"]
 ages = [9, 12, 14, 19]
-height = [4.5, 4.8, 5.8, 5.95]
-male = [false, true, true, true]
+heighth = [4.5, 4.8, 5.8, 5.95]
+males = [false, true, true, true]
+# .pop - "Bob" will be gone
+names.pop
+# .push Added 21
+ages.push(21)
+# .shift 4.5 will be returned but removed permanetly from the array
+heighth.shift
+# .unshift will add true as[0]
+males.unshift(true)
